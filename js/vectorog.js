350d5b78579e6cae238d906db9c54b60e431aebb
Max Klimin <nobus@MacBook-Air-Administrator1-3.local>
2013-06-28 23:31:17 +0400

    Draw background and player.

--- js/vectorog.js
+++ js/vectorog.js
@@ -1,10 +1,10 @@
 
 var drawAll = function() {
-
+	var bg = this.rect(0, 0, 800, 600).attr({"fill": "#5da130"});
+	var txt = this.text(100, 100, "@").attr({"font": "20px Arial"});
 }
 
 $(function() {
 	var screen = Raphael("container", 800, 600, drawAll); 
-
 });
 
