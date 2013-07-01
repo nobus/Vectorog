9fd3a565b2095c128a46e033fd68abe5aabeb12e
Max Klimin <nobus@MacBook-Air-Administrator1-3.local>
2013-07-01 22:54:49 +0400

    Location generator prototype. Path checker prototype.

--- js/vectorog.js
+++ js/vectorog.js
@@ -14,13 +14,96 @@
 
 var player = NaN;
 
-var checkPath = function() {
-	// !!!!!!!!
+// 20 x 15
+var Location = []
+
+var randInt = function() {
+	return Math.random();
+}
+
+var locationGenerator = function() {
+	// 20 x 15
+	// tree = "\u2663"
+	var l = [];
+
+	for (var i = 0; i < 15; i++){
+		l.push([]);
+
+		for (var ii = 0; ii < 20; ii++){
+			var r = randInt();
+
+			if (i == 2 && ii == 2) {
+				continue;
+			}
+
+			if (r > 0.8){
+				l[i].push(2663);
+			} else {
+				l[i].push(0);
+			}
+		}
+	}
+
+	return l;
+}
+
+var drawLocation = function(paper, l) {
+	var x = 20;
+	var y = 20;
+
+	for (var i = 0; i < l.length; i++){
+		for (var ii = 0; ii < l[i].length; ii++){
+			var e = l[i][ii];
+
+			if (e == 2663) {
+				paper.text(x, y, "\u2663").attr({"font": "20px Arial"});
+			}
+
+			x += 40;
+		}
+		x = 20;
+		y += 40;
+	}
+}
+
+var drawGrid = function(paper){
+	var size= 40
+
+	for(var i = 0; i < 800; i = i + size){
+		for (var ii = 0; ii < 600; ii = ii + size){
+			paper.rect(i, ii, size, size);
+		}
+	}
 }
 
 var drawAll = function() {
 	var bg = this.rect(0, 0, 800, 600).attr({"fill": "#5da130"});
-	player = this.text(100, 100, "\u263a").attr({"font": "20px Arial"});
+	drawGrid(this);
+	Location = locationGenerator();
+	drawLocation(this, Location);
+
+	player = this.text(100, 100, "@").attr({"font": "20px Arial"});
+	//tree = this.text(20, 20, "\u2663").attr({"font": "20px Arial"});
+}
+
+var checkPath = function(x, y) {
+	// 0, 0, 800, 600
+	if (x <= 0 || x >= 800){
+		return false;
+	} else if (y <= 0 || y >= 600) {
+		return false;
+	}
+
+	var xx = Math.ceil(x / 40) - 1;
+	var yy = Math.ceil(y / 40) - 1;
+
+	var e = Location[yy][xx];
+
+	if (e == 2663) {
+		return false;
+	}
+
+	return true;
 }
 
 $(function() {
@@ -45,18 +128,27 @@ $(function() {
 		var x = attr["x"];
 		var y = attr["y"];
 
+		var step = 40;
 		if (key == 87 || key == 38) {
 			// up
-			player.attr({"y": y - 10});
+			if (checkPath(x , y - step)){
+				player.attr({"y": y - step});
+			}
 		} else if (key == 83 || key == 40) {
 			// down
-			player.attr({"y": y + 10});
+			if (checkPath(x, y + step)){
+				player.attr({"y": y + step});
+			}
 		} else if (key == 65 || key == 37) {
 			// left
-			player.attr({"x": x - 10});
+			if (checkPath(x - step, y)){
+				player.attr({"x": x - step});
+			}
 		} else if (key == 68 || key == 39) {
 			// right
-			player.attr({"x": x + 10});
+			if (checkPath(x + step, y)){
+				player.attr({"x": x + step});
+			}
 		}
 
 	});
