b197a862e028648ab736062327dc5560e5c8d76a
Max Klimin <nobus@dhcp-2-176-ben.yandex.net>
2013-07-04 20:28:15 +0400

    Add location type. Refactoring.

--- js/maps.js
+++ js/maps.js
@@ -3,47 +3,21 @@ var WorldMap = {
 	locations: []
 }
 
-// deprecated
-WorldMap.locationGenerator = function(px, py) {
-	// 20 x 15
-	// tree = "\u2663"
-	var l = [];
-
-	for (var i = 0; i < 15; i++){
-		l.push([]);
-
-		for (var ii = 0; ii < 20; ii++){
-			var r = randInt();
-
-			if (i == py && ii == px) {
-				l[i].push(0);  // player
-			}
-
-			if (r > 0.8){
-				l[i].push(2663);
-			} else {
-				l[i].push(0);
-			}
-		}
-	}
-
-	return l;
-}
-
 WorldMap.getLocationByLID = function(lid) {
 	return this.locations[lid];
 }
 
-WorldMap.newLocation = function(name, px, py) {
+WorldMap.newLocation = function(name, type, px, py) {
 	// px and py == 1 if palyer not in location
-	var l = new Location(name, px, py);
+	var l = new Location(name, type, px, py);
 	this.locations.push(l);
 
 	return this.locations.length - 1;
 }
 
-var Location = function(name, px, py) {
+var Location = function(name, type, px, py) {
 	this.name = name;
+	this.type = type;
 	this.neighborhood = {};
 	this.maps = this.mapGenerator(px, py);
 }
@@ -51,6 +25,18 @@ var Location = function(name, px, py) {
 Location.prototype.mapGenerator = function(px, py) {
 	// 20 x 15
 	// tree = "\u2663"
+
+	/*
+		types:
+			forest = 0.7
+			thicket = 0.5
+
+
+	*/
+
+	var locationTypes = {"thicket": 0.5, "forest": 0.7, "veld": 0.9};
+	var t = locationTypes[this.type];
+
 	var l = [];
 
 	for (var i = 0; i < 15; i++){
@@ -63,7 +49,7 @@ Location.prototype.mapGenerator = function(px, py) {
 				l[i].push(0);  // player
 			}
 
-			if (r > 0.8){
+			if (r > t){
 				l[i].push(2663);
 			} else {
 				l[i].push(0);
