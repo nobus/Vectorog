5300b7ed42d0dfbd69aa100afd3f960f75f33e5c
Max Klimin <nobus@MacBook-Air-Administrator1-3.local>
2013-07-10 23:33:49 +0400

    Add getRandomInt function.

--- js/common.js
+++ js/common.js
@@ -3,6 +3,10 @@ var randInt = function() {
 	return Math.random();
 }
 
+var getRandomInt = function(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 var storageSupport = function() {
 	try {
     	return 'localStorage' in window && window['localStorage'] !== null;
